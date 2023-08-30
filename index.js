35c2dcc741d4876d89dc91e185762126e3b407fb
EzatRashid <ezat.b.rashid@gmail.com>
2023-08-29 20:49:31 -0600

    finished second function

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 // 1. deposit some money
-// 2. determine #of lines to bet on
-// 3. collect bet amount
-// 4.spin
-// 5.check if the user won
 
 const prompt  = require('prompt-sync')();
 
 const deposit = () =>{
     while(true){
-
         const depositAmount = prompt('Please enter an amount to deposit: $')
         const numberDepositAmount = parseFloat(depositAmount)
         console.log(`Your balance is $${numberDepositAmount}`)
@@ -22,3 +17,23 @@ const deposit = () =>{
 }
 
 const depositAmount = deposit();
+
+// 2. determine #of lines to bet on
+const getLines = () =>{
+    while(true){
+
+        const lines = prompt('Enter number of lines you would like to bet on: ')
+        const NumberOfLines = parseFloat(lines)
+        
+        if(isNaN(NumberOfLines) || NumberOfLines <= 0 || NumberOfLines > 3 ){
+            console.log('invalid line amount, try again: ')
+        }else{
+            return NumberOfLines
+        }
+    }
+}
+
+const NumberOfLines = getLines();
+// 3. collect bet amount
+// 4.spin
+// 5.check if the user won
